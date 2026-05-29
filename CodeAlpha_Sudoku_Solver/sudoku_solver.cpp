c3a96dd299ae20c01362c4e364b084c0c602d947
vinayakgarg1 <vngarg0127@gmail.com>
2026-05-29 13:52:56 +0530

    Sudoku Solver to accept user input

--- CodeAlpha_Sudoku_Solver/sudoku_solver.cpp
+++ CodeAlpha_Sudoku_Solver/sudoku_solver.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <limits>
-
 using namespace std;
-
 const int N = 9;
 
 // Function to print the Sudoku grid
@@ -12,13 +10,9 @@ void printGrid(int grid[N][N]) {
             cout << "------+-------+------\n";
         }
         for (int col = 0; col < N; col++) {
-            if (col % 3 == 0 && col != 0) {
-                cout << "| ";
-            }
-            if (grid[row][col] == 0)
-                cout << ". ";
-            else
-                cout << grid[row][col] << " ";
+            if (col % 3 == 0 && col != 0) cout << "| ";
+            if (grid[row][col] == 0) cout << ". ";
+            else cout << grid[row][col] << " ";
         }
         cout << "\n";
     }
@@ -29,11 +23,9 @@ bool isSafe(int grid[N][N], int row, int col, int num) {
     for (int x = 0; x < N; x++) {
         if (grid[row][x] == num) return false;
     }
-
     for (int x = 0; x < N; x++) {
         if (grid[x][col] == num) return false;
     }
-
     int startRow = row - row % 3;
     int startCol = col - col % 3;
     for (int i = 0; i < 3; i++) {
@@ -41,21 +33,17 @@ bool isSafe(int grid[N][N], int row, int col, int num) {
             if (grid[i + startRow][j + startCol] == num) return false;
         }
     }
-
     return true;
 }
 
 // Backtracking Solver
 bool solveSudoku(int grid[N][N], int row, int col) {
     if (row == N - 1 && col == N) return true;
-
     if (col == N) {
         row++;
         col = 0;
     }
-
     if (grid[row][col] != 0) return solveSudoku(grid, row, col + 1);
-
     for (int num = 1; num <= 9; num++) {
         if (isSafe(grid, row, col, num)) {
             grid[row][col] = num;
@@ -68,13 +56,11 @@ bool solveSudoku(int grid[N][N], int row, int col) {
 
 int main() {
     int grid[N][N];
-
     cout << "===================================================\n";
     cout << "                  SUDOKU SOLVER                    \n";
     cout << "===================================================\n";
     cout << "Enter your Sudoku puzzle row by row.\n";
     cout << "Enter 9 numbers per row separated by spaces (use 0 for empty cells):\n\n";
-
     // Getting grid input from user
     for (int i = 0; i < N; i++) {
         cout << "Row " << (i + 1) << ": ";
@@ -86,19 +72,14 @@ int main() {
             }
         }
     }
-
     cout << "\n---------------------------------------------------\n";
     cout << "Your Entered Puzzle:\n";
     printGrid(grid);
     cout << "---------------------------------------------------\n\n";
-
     if (solveSudoku(grid, 0, 0)) {
         cout << "Puzzle Solved Successfully:\n";
         printGrid(grid);
-    } else {
-        cout << "[ERROR] No solution exists for this Sudoku puzzle.\n";
-    }
-
+    } else cout << "[ERROR] No solution exists for this Sudoku puzzle.\n";
     cout << "===================================================\n";
     return 0;
 }
